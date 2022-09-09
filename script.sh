ba08717a854bbe3d87b5111040eb27fc7b9f8675
Akanksha Kutal <akankshk@thoughtworks.com>
2022-09-09 15:04:46 +0530

    Local rate limit header filter

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 x=1
-while [ $x -le 110 ]
+while [ $x -le 10 ]
 do
-  curl -I  "productpage:9080/productpage" | grep "HTTP/1.1"
+  curl -I --location --request GET 'http://127.0.0.1:80/productpage' --header 'user-id: a'
   x=$(( $x + 1 ))
 done
